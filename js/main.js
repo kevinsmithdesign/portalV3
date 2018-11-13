f8d5b0e4274fefd1b17639cdeca6e883447b252e
Kevin Smith <kevsmith@hou-kevsmith-m.local>
2018-11-13 16:13:54 -0600

    cleaning up js and starting to adding animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 $(function() {
-    console.log( "ready!" );
-
-    // Nav Btns 
-
-
-
+    // Nav Btns Code STARTS 
     $('.dashboardBtn').css({'background': '#3399FF', 
                             'border':'none',
                             'border-radius':'20px',
@@ -47,32 +42,27 @@ $(function() {
                           'color': 'white',
                           'margin':'46px 10px 0 10px', 
                           'cursor': 'pointer'});
+    // Nav Btns Code ENDS 
 
 
 
-    // Nav Dropdown Logic Starts
+    // Nav Dropdown Logic STARTS
     $('.navDropdownContainer').hide();
-    
     $('.hamburgerMenu').click(function(){
         $('.navDropdownContainer').toggle();
     });
+    // Nav Dropdown Logic ENDS
 
+    // More Dropdown Code STARTS 
     $('.moreDropdownContainer').hide();
-
     $('.moreBtnContainer').click(function(){
         $('.moreDropdownContainer').toggle();
     });
-
-    // Up Blink on Status Bar
-    // var blink = new TimelineMax();
-    // blink.to('.up', 1, {opacity: .2, repeat: 2})
-    //      .to('.up', 1, {opacity: 1});
-
-    
+    // More Dropdown Code ENDS
 
 
 
-    // Status Progress Bar Chart Animation
+    // Status Progress Bar Chart Animation STARTS 
     var tl = new TimelineMax();
 
     tl.to('.barFill1', .2, {  background:'#0080FF', height: '30px'})
@@ -82,26 +72,50 @@ $(function() {
     .to('.barFill5', .2, { background: '#0080FF', height: '26px'})
     .to('.barFill6', .2, { background: '#0080FF', height: '53px'})
     .to('.barFill7', .2, { background: '#0080FF', height: '79px'});
+    // Status Progress Bar Chart Animation ENDS 
 
-    var defaultImg = new TimelineMax();
 
-    // defaultImg.from('.defaultCard', 1, { opacity:0})
-    // $('.defaultCard')
 
-    // defaultImg.to('.defaultCard', 3, { opacity: 0 })
+    // Content Placeholder Code STARTS
+    // var defaultImg = new TimelineMax();
 
     function showDefaultCard() {
         $('.defaultCard').show();
         $('.dataLoadedCard').hide();
     }
-
     function showDataLoaded() {
         $('.defaultCard').hide();
         $('.dataLoadedCard').show();
     }
-
     setTimeout(showDefaultCard, 0);
-    setTimeout(showDataLoaded, 2000)
+    setTimeout(showDataLoaded, 2000);
+
+    // Content Placeholder Code ENDS 
+
+
+    // Backup Alert STARTS
+    $('.alertContainer').hide();
+
+    function showBackupAlert() {
+        $('.alertContainer').show();
+        var backupAlert = new TimelineLite({pased: true});
+        backupAlert.from('.alertbgOne', .2, { opacity:0, x: 50, scale: .5 })
+                    .from('.alertbgTwo', .2, { opacity:0, x: 50, scale: .5 })
+                    .from('.alertbgThree', .2, { opacity:0, x: 50, scale: .5 });
+        $( ".click" ).click(function() {
+            backupAlert.to('.alertbgOne', .2, { x: 20 })
+                       .to('.alertbgTwo', .2, { x: 40 })
+                       .to('.alertbgThree', .2, { x: 60 })
+                       .to('.alertContainer', .2, { x: 1000 })
+                       .to('.bgHeaderColor', 1, { backgroundColor: '#3580F6'})
+                       .to('.bgHeaderColor', 1, { backgroundColor: '#006CD6'});
+                       
+        });
+    }
+    setTimeout(showBackupAlert, 2100);
+ 
+
+    // Backup Alert ENDS
 
  
 
